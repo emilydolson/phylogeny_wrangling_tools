35466d4559a17a64c576ca4579a7b0f53218d918
Emily Dolson <emilyldolson@gmail.com>
2023-03-02 22:03:20 -0500

    Add full pipeline script

---
+++ symbulation_to_abstract_newick_pipeline.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+python abstract_phylogenies.py $1 $2 $3
+python make_links.py test.csv
+
+symfile=`basename $1`
+hostfile=`basename $2`
+
+alifedata-phyloinformatics-convert fromalifedata --input-file compressed_${symfile} --output-file symtree.nwk --output-schema newick --suppress-unifurcations
+alifedata-phyloinformatics-convert fromalifedata --input-file compressed_${hostfile} --output-file hosttree.nwk --output-schema newick --suppress-unifurcations
