ba64bb2e40d053522e2c29262ea3ef8d5e4547b0
Emily Dolson <emilyldolson@gmail.com>
2023-03-03 20:09:28 -0500

    Add more tests

--- test_scripts.py
+++ test_scripts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import ALifeStdDev.phylogeny as phylodev
+from pytest import approx
 from .make_links import *
 from .abstract_phylogenies import *
 
@@ -40,7 +41,28 @@ def test_abstract_phylogeny():
     assert conversion_dict[11] != 0
     assert conversion_dict[11] != conversion_dict[5]
     assert conversion_dict[5] != 0
+    assert conversion_dict[12] != 0
+    assert conversion_dict[12] != conversion_dict[5]
+    assert conversion_dict[12] != conversion_dict[11]
+    assert conversion_dict[13] != 0
+    assert conversion_dict[13] != conversion_dict[5]
+    assert conversion_dict[13] != conversion_dict[11]
+    assert conversion_dict[13] != conversion_dict[12]
+    assert conversion_dict[13] == conversion_dict[14]
+    assert conversion_dict[13] == conversion_dict[15]
 
     # Check correspondence
     assert set(df["id"]) == set(conversion_dict.values())
     assert set(original_df.index) == set(conversion_dict.keys())
+
+    # Check topology
+    # Should have 3 rows
+    assert (df.size / len(df.columns)) == 5
+    assert df.loc[0, "ancestor_list"] == [None]
+    assert df.loc[1, "ancestor_list"] == [0]
+    assert df.loc[2, "ancestor_list"] == [0]
+    assert df.loc[3, "ancestor_list"] == [3]
+    assert df.loc[4, "ancestor_list"] == [3]
+    assert df.loc[0, "edge_length"] == approx(0)
+    assert df.loc[1, "edge_length"] == approx(.07)
+    assert df.loc[2, "edge_length"] == approx(.07)
